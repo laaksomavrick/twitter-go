720b423be97be92968f2ec4eea18d09b55496504
laakso.mavrick@gmail.com <laakso.mavrick@gmail.com>
2019-04-21 19:29:12 -0400

    adding common

---
+++ common/common.go
@@ -0,0 +1,7 @@
+package common
+
+import (
+	"github.com/aws/aws-lambda-go/events"
+)
+
+type Event events.APIGatewayProxyRequest
---
+++ common/go.mod
@@ -0,0 +1,5 @@
+module github.com/laaksomavrick/twitter-go/common
+
+go 1.12
+
+require github.com/aws/aws-lambda-go v1.10.0
